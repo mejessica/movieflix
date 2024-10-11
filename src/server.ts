250155d3091f56b58f7e7c2cb66074a1592a30a6
jessica <jegelsdorf@gmail.com>
2024-10-10 21:26:05 -0300

    rotas de llistagem e criação de filmes

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,52 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 
-const port = 3000; 
+const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
-app.get('/', (req, res)=>{
+app.use(express.json())
+
+app.get('/', (req, res) => {
     res.send('home pagr');
 })
 
-app.get('/movies', async (req, res)=>{
-   const movies = await prisma.filme.findMany()
-   res.json(movies);
+app.get('/movies', async (_, res) => {
+    const movies = await prisma.filme.findMany({
+        orderBy: {
+            titulo: 'asc'
+        },
+        include: {
+            generos: true,
+            elenco_filme: true
+        }
+    })
+    res.json(movies);
 })
 
+app.post('/movies', async (req, res) => {
 
-app.listen(port, ()=>{
+    const { titulo, ano_lancamento, duracao_minutos, classificacao_etaria, diretor_id, genero_id, sinopse } = req.body;
+
+    try {
+        await prisma.filme.create({
+            data: {
+                titulo,
+                ano_lancamento, //new Date(2020, 0, 1)
+                duracao_minutos,
+                classificacao_etaria,
+                diretor_id,
+                genero_id,
+                sinopse
+            }
+        });
+    }catch(error){
+        return res.status(500).send({message: 'falha ao cadastrar um filme'})
+    }
+
+    res.status(201).send()
+})
+
+app.listen(port, () => {
     console.log('servidor ligado');
 })
